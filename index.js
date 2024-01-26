7b1ad4409aa05b659854650de9a010ab053c3d8b
HowToRespawn <141860098+howtorespawn@users.noreply.github.com>
2024-01-26 02:53:14 +0000

    two players

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-console.log("e");
-
+let i = 1;
 function playerMark(className) {
-    document.getElementById(className).innerHTML = "X";
+    if (i % 2) {
+        document.getElementById(className).innerHTML = "X";
+    } else {
+    document.getElementById(className).innerHTML = "O";
+    }
+    i++;
+    // document.getElementById(className).innerHTML = "X";
+    // "X" = gameArray
 }
-// document.getElementById("er").innerHTML = "d";
-// document.getElementById("boxtxt").innerHTML = "D";
+
+let gameArray=[
+    {
+        topLeft:    "",
+        topMiddle:  "",
+        topRight:   "",
+    },
+    {
+        middleLeft: "",
+        middleMiddle:"",
+        middleRight:"",
+    },
+    {
+        bottomLeft: "",
+        bottomMiddle:"",
+        bottomRight:"",
+    },
+]
+console.log(gameArray[0].topLeft);
