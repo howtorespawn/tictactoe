6ecabed269e05154503733011f7f15a1f9c84640
HowToRespawn <141860098+howtorespawn@users.noreply.github.com>
2024-01-26 19:42:37 +0000

    added reset and fix bugs

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 let i = 1;
+// const checkBox = document.getElementById("cb").innerText;
+// console.log(checkBox);
+// const classNameText = document.getElementById(className).innerHTML;
 function playerMark(className) {
-    if (i % 2) {
-        document.getElementById(className).innerHTML = "X";
-    } else {
-    document.getElementById(className).innerHTML = "O";
+    const classNameText = document.getElementById(className).innerHTML;
+    if (classNameText == ""){
+        if (i % 2) {
+            document.getElementById(className).innerHTML = "X";
+        } else {
+            document.getElementById(className).innerHTML = "O";
+        }
+        i++;
     }
-    i++;
+    else{}
     // document.getElementById(className).innerHTML = "X";
     // "X" = gameArray
 }
-
-let gameArray=[
-    {
-        topLeft:    "",
-        topMiddle:  "",
-        topRight:   "",
-    },
-    {
-        middleLeft: "",
-        middleMiddle:"",
-        middleRight:"",
-    },
-    {
-        bottomLeft: "",
-        bottomMiddle:"",
-        bottomRight:"",
-    },
-]
-console.log(gameArray[0].topLeft);
+function resetGame() {
+    // document.getElementsByClassName("checkbox").innerText = "";
+    // console.log("e")
+    location.reload()
+}
+// let gameArray=[
+//     {
+//         topLeft:    "",
+//         topMiddle:  "",
+//         topRight:   "",
+//     },
+//     {
+//         middleLeft: "",
+//         middleMiddle:"",
+//         middleRight:"",
+//     },
+//     {
+//         bottomLeft: "",
+//         bottomMiddle:"",
+//         bottomRight:"",
+//     },
+// ]
+// console.log(gameArray[0].topLeft);
