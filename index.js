86828dae55ccb177bc51e70445926c8a63b62aca
HowToRespawn <141860098+howtorespawn@users.noreply.github.com>
2024-01-26 02:46:47 +0000

    added marking system

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("e");
+
+function playerMark(className) {
+    document.getElementById(className).innerHTML = "X";
+}
+// document.getElementById("er").innerHTML = "d";
+// document.getElementById("boxtxt").innerHTML = "D";
